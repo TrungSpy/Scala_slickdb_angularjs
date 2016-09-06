2752f6255a7a80008e34cb36f7df0f5b1be29a95
PhanVanTrung <vantrungphan2012@gmail.com>
2016-09-06 22:59:49 +0900

    1st commit

---
+++ conf/sample/database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "employee"("id" SERIAL PRIMARY KEY ,"name" varchar(200) , "email" varchar(200)  ,"company_name" varchar,"position" varchar);
+3INSERT INTO "employee" values (1,'Vikas', 'vikas@knoldus.com','Knoldus','CTO');
+4INSERT INTO "employee" values (2,'Bhavya', 'bhavya@knoldus.com','Knoldus','Senior Director');
+5INSERT INTO "employee" values (3,'Ayush', 'ayush@knoldus.com','Knoldus','Lead Consultant');
+6INSERT INTO "employee" values (4,'Satendra', 'satendra@knoldus.com','Knoldus','Senior Consultant');
